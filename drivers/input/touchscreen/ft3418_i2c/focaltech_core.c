a29b4eaaf70b633f523dccc19091df3f28f3d67e
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-05 19:37:47 +0530

    input/ts: ft3418_i2c: Switch to async probe
     * slightly reduces boot times
    
    Signed-off-by: Fiqri Ardyansyah <fiqri15072019@gmail.com>

--- drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
+++ drivers/input/touchscreen/ft3418_i2c/focaltech_core.c
@@ -2066,6 +2066,7 @@ static struct i2c_driver fts_ts_driver = {
         .pm = &fts_dev_pm_ops,
 #endif
         .of_match_table = of_match_ptr(fts_dt_match),
+        .probe_type = PROBE_PREFER_ASYNCHRONOUS,
     },
     .id_table = fts_ts_id,
 };
